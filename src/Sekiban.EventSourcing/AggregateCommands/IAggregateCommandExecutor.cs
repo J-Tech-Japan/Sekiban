6504bc9b2c8053478b39a8a07c43f201e7283fe3
Tomohisa Takaoka <ttakaoka@jtechs.com>
2022-05-08 09:22:16 -0700

    Update IAggregateCommandExecutor.cs

--- src/Sekiban.EventSourcing/AggregateCommands/IAggregateCommandExecutor.cs
+++ src/Sekiban.EventSourcing/AggregateCommands/IAggregateCommandExecutor.cs
@@ -2,8 +2,28 @@ namespace Sekiban.EventSourcing.AggregateCommands;
 
 public interface IAggregateCommandExecutor
 {
+    /// <summary>
+    ///     集約コマンドを実行する
+    ///     こちらのメソッドは集約の変更機能のメソッドとなります。
+    /// </summary>
+    /// <param name="command">対象集約コマンド</param>
+    /// <param name="callHistories">呼び出し履歴 APIなどから直接コマンドを呼ぶ場合はnullで良い。他のイベントやコマンドからコマンドを呼ぶ際に、呼出履歴をつける</param>
+    /// <typeparam name="T">集約クラス</typeparam>
+    /// <typeparam name="Q">DTOクラス</typeparam>
+    /// <typeparam name="C">コマンドクラス</typeparam>
+    /// <returns></returns>
     Task<AggregateCommandExecutorResponse<Q, C>> ExecChangeCommandAsync<T, Q, C>(C command, List<CallHistory>? callHistories = null)
         where T : TransferableAggregateBase<Q> where Q : AggregateDtoBase, new() where C : ChangeAggregateCommandBase<T>;
+    /// <summary>
+    ///     集約コマンドを実行する
+    ///     こちらのメソッドは集約の新規作成機能のメソッドとなります。
+    /// </summary>
+    /// <param name="command">対象集約コマンド</param>
+    /// <param name="callHistories">呼び出し履歴 APIなどから直接コマンドを呼ぶ場合はnullで良い。他のイベントやコマンドからコマンドを呼ぶ際に、呼出履歴をつける</param>
+    /// <typeparam name="T">集約クラス</typeparam>
+    /// <typeparam name="Q">DTOクラス</typeparam>
+    /// <typeparam name="C">コマンドクラス</typeparam>
+    /// <returns></returns>
     Task<AggregateCommandExecutorResponse<Q, C>> ExecCreateCommandAsync<T, Q, C>(C command, List<CallHistory>? callHistories = null)
         where T : TransferableAggregateBase<Q> where Q : AggregateDtoBase, new() where C : ICreateAggregateCommand<T>;
 }
