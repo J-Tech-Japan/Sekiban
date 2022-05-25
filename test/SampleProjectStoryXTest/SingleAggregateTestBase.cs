e815111b69744b39839c37e7aa8c7f2f4810d6b3
Tomohisa Takaoka <ttakaoka@jtechs.com>
2022-05-24 18:43:59 -0700

    テストの調整

--- test/SampleProjectStoryXTest/SingleAggregateTestBase.cs
+++ test/SampleProjectStoryXTest/SingleAggregateTestBase.cs
@@ -1,13 +1,16 @@
 using ESSampleProjectDependency;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Sekiban.EventSourcing.Aggregates;
+using Sekiban.EventSourcing.TestHelpers;
 using System;
 using Xunit;
 namespace SampleProjectStoryXTest;
 
 [Collection("Sequential")]
-public class SingleAggregateTestBase : IDisposable
+public class SingleAggregateTestBase<TAggregate, TDto> : IDisposable where TAggregate : TransferableAggregateBase<TDto> where TDto : AggregateDtoBase
 {
+    protected readonly AggregateTestHelper<TAggregate, TDto> _helper;
     protected readonly ServiceProvider _serviceProvider;
     public SingleAggregateTestBase()
     {
@@ -18,6 +21,8 @@ public class SingleAggregateTestBase : IDisposable
         Dependency.RegisterForAggregateTest(services);
 
         _serviceProvider = services.BuildServiceProvider();
+
+        _helper = new AggregateTestHelper<TAggregate, TDto>(_serviceProvider);
     }
     public void Dispose() { }
     public T GetService<T>()
