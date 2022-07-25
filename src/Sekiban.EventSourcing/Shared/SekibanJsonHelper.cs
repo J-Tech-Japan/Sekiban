3f29d7124c1221344b57727889ba1274134423ea
Takashi Kawae <kawae@jtechs.com>
2022-07-25 13:51:28 +0700

    System.Text.Json切り替え用のクラスを追加

---
+++ src/Sekiban.EventSourcing/Shared/SekibanJsonHelper.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace Sekiban.EventSourcing.Shared;
+
+public static class SekibanJsonHelper
+{
+    public static JsonSerializerOptions GetDefaultJsonSerializerOptions() => new()
+    {
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNameCaseInsensitive = true,
+    };
+
+    public static string? Serialize(dynamic? obj)
+    {
+        if (obj is null)
+            return null;
+
+        return JsonSerializer.Serialize(obj, GetDefaultJsonSerializerOptions());
+    }
+
+    public static string? Serialize(Exception ex)
+    {
+        return Serialize(new
+        {
+            ex.Message,
+            ex.Source,
+            StackTrace = ex.StackTrace?.ToString(),
+        });
+    }
+
+    public static object? Deserialize(string? jsonString, Type returnType)
+    {
+        if (string.IsNullOrEmpty(jsonString))
+            return default;
+
+        return JsonSerializer.Deserialize(jsonString, returnType);
+    }
+
+    public static T? Deserialize<T>(string? jsonString)
+    {
+        if (string.IsNullOrEmpty(jsonString))
+            return default;
+
+        return JsonSerializer.Deserialize<T>(jsonString, GetDefaultJsonSerializerOptions());
+    }
+
+    public static T? ConvertTo<T>(dynamic? jsonObj)
+    {
+        if (jsonObj is null)
+            return default;
+
+        var jsonString = Serialize(jsonObj);
+        return Deserialize<T>(jsonString);
+    }
+
+    public static T? GetValue<T>(dynamic? jsonObj, string propertyName)
+    {
+        var jsonString = Serialize(jsonObj);
+        return JsonNode.Parse(jsonString,
+            new JsonNodeOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+            })
+            ?[propertyName]?.GetValue<T>();
+    }
+}
