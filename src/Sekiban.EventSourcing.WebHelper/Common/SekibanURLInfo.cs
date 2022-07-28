43ba91f49fc4e4b50ed530e1b30788854757ef8b
Tomohisa Takaoka <ttakaoka@jtechs.com>
2022-07-28 10:51:23 -0700

    インデックス機能

--- src/Sekiban.EventSourcing.WebHelper/Common/SekibanURLInfo.cs
+++ src/Sekiban.EventSourcing.WebHelper/Common/SekibanURLInfo.cs
@@ -2,6 +2,7 @@ namespace Sekiban.EventSourcing.WebHelper.Common;
 
 public record SekibanURLInfo
 {
+    public string Method { get; set; }
     public string Url { get; init; }
-    public string CommandType { get; init; }
+    public string JsonBodyType { get; init; }
 }
--- src/Sekiban.EventSourcing.WebHelper/Controllers/SekibanApiListController.cs
+++ src/Sekiban.EventSourcing.WebHelper/Controllers/SekibanApiListController.cs
@@ -17,7 +17,7 @@ public class SekibanApiListController<T> : ControllerBase
 
     [HttpGet]
     [Route("createCommands")]
-    public async Task<ActionResult<List<SekibanURLInfo>>> CreateCommandList()
+    public async Task<ActionResult<List<SekibanURLInfo>>> CreateCommandListAsync()
     {
         await Task.CompletedTask;
         var list = new List<SekibanURLInfo>();
@@ -35,9 +35,67 @@ public class SekibanApiListController<T> : ControllerBase
                     new SekibanURLInfo
                     {
                         Url = $"/{_sekibanControllerOptions.CreateCommandPrefix}/{aggregateType.Name}/{commandType.Name}",
-                        CommandType = commandType.Name
+                        JsonBodyType = commandType.Name,
+                        Method = "POST"
                     });
             }
+        }
+        return Ok(list);
+    }
+
+    [HttpGet]
+    [Route("changeCommands")]
+    public async Task<ActionResult<List<SekibanURLInfo>>> ChangeCommandListAsync()
+    {
+        await Task.CompletedTask;
+        var list = new List<SekibanURLInfo>();
+        foreach (var (serviceType, implementationType) in _sekibanControllerItems.SekibanCommands)
+        {
+            var interfaceType = serviceType;
+
+            if (interfaceType?.Name == typeof(IChangeAggregateCommandHandler<,>).Name)
+            {
+                var aggregateType = interfaceType?.GenericTypeArguments[0];
+                var commandType = interfaceType?.GenericTypeArguments[1];
+                var aggregateContentsType = aggregateType?.BaseType?.GenericTypeArguments[0];
+                if (aggregateType == null || commandType == null || aggregateContentsType == null) { continue; }
+                list.Add(
+                    new SekibanURLInfo
+                    {
+                        Url = $"/{_sekibanControllerOptions.ChangeCommandPrefix}/{aggregateType.Name}/{commandType.Name}",
+                        JsonBodyType = commandType.Name,
+                        Method = "PATCH"
+                    });
+            }
+        }
+        return Ok(list);
+    }
+
+    [HttpGet]
+    [Route("getQueries")]
+    public async Task<ActionResult<List<SekibanURLInfo>>> GetQueryListAsync()
+    {
+        await Task.CompletedTask;
+        var list = new List<SekibanURLInfo>();
+        foreach (var aggregateType in _sekibanControllerItems.SekibanAggregates)
+        {
+            var aggregateContentsType = aggregateType?.BaseType?.GenericTypeArguments[0];
+            if (aggregateType == null || aggregateContentsType == null) { continue; }
+            list.Add(new SekibanURLInfo { Url = $"/{_sekibanControllerOptions.QueryPrefix}/{aggregateType.Name}/{{id}}", Method = "GET" });
+        }
+        return Ok(list);
+    }
+    [HttpGet]
+    [Route("listQueries")]
+    public async Task<ActionResult<List<SekibanURLInfo>>> GetListQueryListAsync()
+    {
+        await Task.CompletedTask;
+        var list = new List<SekibanURLInfo>();
+        foreach (var aggregateType in _sekibanControllerItems.SekibanAggregates)
+        {
+            var aggregateContentsType = aggregateType?.BaseType?.GenericTypeArguments[0];
+            if (aggregateType == null || aggregateContentsType == null) { continue; }
+            list.Add(new SekibanURLInfo { Url = $"/{_sekibanControllerOptions.QueryPrefix}/{aggregateType.Name}", Method = "GET" });
         }
         return Ok(list);
     }
