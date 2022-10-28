90c2cb24b0e1103e9250239bd845a9f13ff6a024
Tomohisa Takaoka <ttakaoka@jtechs.com>
2022-10-29 06:37:43 +0900

    SekibanValidationExceptionの移動

---
+++ src/Sekiban.Core/Exceptions/SekibanValidationErrorsException.cs
@@ -0,0 +1,17 @@
+using Sekiban.Core.Validation;
+using System.ComponentModel.DataAnnotations;
+namespace Sekiban.Core.Exceptions;
+
+public class SekibanValidationErrorsException : Exception, ISekibanException
+{
+
+    public SekibanValidationErrorsException(IEnumerable<SekibanValidationParameterError> errors)
+    {
+        Errors = errors;
+    }
+    public SekibanValidationErrorsException(IEnumerable<ValidationResult> validationResults)
+    {
+        Errors = SekibanValidationParameterError.CreateFromValidationResults(validationResults);
+    }
+    public IEnumerable<SekibanValidationParameterError> Errors { get; init; }
+}
