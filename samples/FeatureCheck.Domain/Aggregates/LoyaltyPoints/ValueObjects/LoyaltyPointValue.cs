7d28ef3cfb09484b03bb319e66ce268d21434b1d
Tomohisa Takaoka <ttakaoka@jtechs.com>
2022-12-09 07:08:50 +0900

    sample that uses value object

---
+++ samples/FeatureCheck.Domain/Aggregates/LoyaltyPoints/ValueObjects/LoyaltyPointValue.cs
@@ -0,0 +1,17 @@
+namespace FeatureCheck.Domain.Aggregates.LoyaltyPoints.ValueObjects;
+
+public record LoyaltyPointValue
+{
+    public LoyaltyPointValue(int value)
+    {
+        if (value < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), "Loyalty point value cannot be negative.");
+        }
+        Value = value;
+    }
+    public int Value { get; init; }
+    public static implicit operator int(LoyaltyPointValue vo) => vo.Value;
+
+    public static implicit operator LoyaltyPointValue(int v) => new LoyaltyPointValue(v);
+}
