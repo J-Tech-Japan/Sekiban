11687c5a54fb5d0a418e2b33187e4831a8aff507
MOTOO11 <2063244+MOTOO11@users.noreply.github.com>
2022-12-22 16:25:55 +0900

    Create TargetAggregatePayloadCollection.cs

---
+++ src/Sekiban.Core/Query/TargetAggregatePayloadCollection.cs
@@ -0,0 +1,14 @@
+﻿using Sekiban.Core.Aggregate;
+using System.Collections.Immutable;
+
+namespace Sekiban.Core.Query.MultiProjections;
+
+public class TargetAggregatePayloadCollection
+{
+    private ImmutableList<IAggregatePayload> TargetAggregatePayloads { get; set; } = ImmutableList<IAggregatePayload>.Empty;
+    public TargetAggregatePayloadCollection Add(IAggregatePayload aggregate)
+    {
+        TargetAggregatePayloads.Add(aggregate);
+        return this;
+    }
+}
