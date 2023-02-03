d3f2bce27d00c5c9ebc6fec2419b1415896afef2
Tomohisa Takaoka <ttakaoka@jtechs.com>
2023-02-02 21:37:03 -0800

    Update AggregateSubtypeTest.cs

--- test/Sekiban.Consistency.Test.CosmosDb/Stories/AggregateSubtypeTest.cs
+++ test/Sekiban.Consistency.Test.CosmosDb/Stories/AggregateSubtypeTest.cs
@@ -2,7 +2,6 @@ using FeatureCheck.Domain.Aggregates.SubTypes.InterfaceBaseTypes;
 using FeatureCheck.Domain.Aggregates.SubTypes.InterfaceBaseTypes.SubAggregates.PurchasedCarts;
 using FeatureCheck.Domain.Aggregates.SubTypes.InterfaceBaseTypes.SubAggregates.ShoppingCarts;
 using FeatureCheck.Domain.Aggregates.SubTypes.InterfaceBaseTypes.SubAggregates.ShoppingCarts.Commands;
-using Microsoft.Extensions.Caching.Memory;
 using Sekiban.Core.Aggregate;
 using Sekiban.Core.Command;
 using Sekiban.Core.Documents;
@@ -18,26 +17,17 @@ namespace SampleProjectStoryXTest.Stories;
 public class AggregateSubtypeTest : TestBase
 {
     private readonly CosmosDbFactory _cosmosDbFactory;
-    private readonly IDocumentPersistentRepository _documentPersistentRepository;
-    private readonly HybridStoreManager _hybridStoreManager;
-    private readonly InMemoryDocumentStore _inMemoryDocumentStore;
-    private readonly IMemoryCache _memoryCache;
-    private readonly ITestOutputHelper _testOutputHelper;
     private readonly IAggregateLoader aggregateLoader;
+    private readonly Guid cartId = Guid.NewGuid();
     private readonly ICommandExecutor commandExecutor;
-
+    private CommandExecutorResponseWithEvents commandResponse = default!;
     public AggregateSubtypeTest(SekibanTestFixture sekibanTestFixture, ITestOutputHelper testOutputHelper) : base(
         sekibanTestFixture,
         testOutputHelper)
     {
-        _testOutputHelper = testOutputHelper;
         _cosmosDbFactory = GetService<CosmosDbFactory>();
         commandExecutor = GetService<ICommandExecutor>();
         aggregateLoader = GetService<IAggregateLoader>();
-        _documentPersistentRepository = GetService<IDocumentPersistentRepository>();
-        _inMemoryDocumentStore = GetService<InMemoryDocumentStore>();
-        _hybridStoreManager = GetService<HybridStoreManager>();
-        _memoryCache = GetService<IMemoryCache>();
     }
 
     [Fact(DisplayName = "SubtypeのAggregateを作成する")]
@@ -52,15 +42,16 @@ public class AggregateSubtypeTest : TestBase
         await _cosmosDbFactory.DeleteAllFromAggregateFromContainerIncludes(DocumentType.Command);
 
 
-        var cartId = Guid.NewGuid();
 
-        var commandResponse = await commandExecutor.ExecCommandWithEventsAsync(
+        commandResponse = await commandExecutor.ExecCommandWithEventsAsync(
             new AddItemToShoppingCartI
                 { CartId = cartId, Code = "TEST1", Name = "Name1", Quantity = 1 });
         Assert.Equal(1, commandResponse.Version);
         Assert.Single(commandResponse.Events);
         Assert.Equal(nameof(ICartAggregate), commandResponse.Events.First().AggregateType);
 
+
+        // this time, the aggregate is created as ShoppingCartI
         var aggregateAsCart = await aggregateLoader.AsAggregateAsync<ICartAggregate>(cartId);
         Assert.NotNull(aggregateAsCart);
         Assert.True(aggregateAsCart.GetPayloadTypeIs<ShoppingCartI>());
@@ -76,13 +67,68 @@ public class AggregateSubtypeTest : TestBase
         Assert.NotNull(aggregateAsCartState2);
         Assert.True(aggregateAsCartState2.Payload is ShoppingCartI);
 
+        // Parent interface can get state as child interface
         var aggregateAsCartState3 = await aggregateLoader.AsDefaultStateAsync<ICartAggregate>(cartId);
         Assert.NotNull(aggregateAsCartState3);
+        Assert.Equal(typeof(ShoppingCartI), aggregateAsCartState3.Payload.GetType());
         var aggregateAsShoppingCartState2 = await aggregateLoader.AsDefaultStateAsync<ShoppingCartI>(cartId);
         Assert.NotNull(aggregateAsShoppingCartState2);
+        // This time, aggregate payload is ShippingCartI, so it will return null
         var aggregateAsPurchasedCartState = await aggregateLoader.AsDefaultStateAsync<PurchasedCartI>(cartId);
         Assert.Null(aggregateAsPurchasedCartState);
 
+
+
+    }
+    [Fact]
+    public async Task SecondCommandTest()
+    {
+        await CosmosDbStory();
+        var purchasedTime = DateTime.Now;
+        commandResponse = await commandExecutor.ExecCommandWithEventsAsync(
+            new SubmitOrderI
+                { CartId = cartId, OrderSubmittedLocalTime = purchasedTime, ReferenceVersion = commandResponse.Version });
+
+
+        // this time, the aggregate is created as ShoppingCartI
+        var aggregateAsCart = await aggregateLoader.AsAggregateAsync<ICartAggregate>(cartId);
+        Assert.NotNull(aggregateAsCart);
+        Assert.True(aggregateAsCart.GetPayloadTypeIs<PurchasedCartI>());
+        var aggregateAsCartState = aggregateAsCart.ToState();
+        Assert.NotNull(aggregateAsCartState);
+        Assert.True(aggregateAsCartState.Payload is PurchasedCartI);
+        Assert.Equal(purchasedTime, aggregateAsCart.ToState<PurchasedCartI>().Payload.PurchasedDate);
+
+        var aggregateAsShoppingCart = await aggregateLoader.AsAggregateAsync<ShoppingCartI>(cartId);
+        Assert.NotNull(aggregateAsShoppingCart);
+        Assert.False(aggregateAsShoppingCart.GetPayloadTypeIs<ShoppingCartI>());
+
+        var aggregateAsCartState2 = aggregateAsShoppingCart.ToState<ICartAggregate>();
+        Assert.NotNull(aggregateAsCartState2);
+        Assert.True(aggregateAsCartState2.Payload is PurchasedCartI);
+
+        var aggregateAsPurchasedCart = await aggregateLoader.AsAggregateAsync<PurchasedCartI>(cartId);
+        Assert.NotNull(aggregateAsPurchasedCart);
+        Assert.True(aggregateAsPurchasedCart.GetPayloadTypeIs<PurchasedCartI>());
+
+        var aggregateAsCartState3 = aggregateAsShoppingCart.ToState<ICartAggregate>();
+        Assert.NotNull(aggregateAsCartState3);
+        Assert.True(aggregateAsCartState3.Payload is PurchasedCartI);
+
+    }
+
+    [Fact]
+    public async Task AfterChangePayloadType()
+    {
+        await SecondCommandTest();
+
+        await Assert.ThrowsAnyAsync<Exception>(
+            async () =>
+            {
+                commandResponse = await commandExecutor.ExecCommandWithEventsAsync(
+                    new AddItemToShoppingCartI
+                        { CartId = cartId, Code = "TEST2", Name = "Name2", Quantity = 2 });
+            });
 
     }
 }
