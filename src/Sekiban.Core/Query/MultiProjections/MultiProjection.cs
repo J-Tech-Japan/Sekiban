1fbc684b761160597126379c142164dc46691772
Tomohisa Takaoka <ttakaoka@jtechs.com>
2023-02-08 00:49:25 -0800

    リファクタリング

--- src/Sekiban.Core/Query/MultiProjections/MultiProjection.cs
+++ src/Sekiban.Core/Query/MultiProjections/MultiProjection.cs
@@ -1,17 +1,17 @@
 using Sekiban.Core.Documents.ValueObjects;
 using Sekiban.Core.Events;
 using Sekiban.Core.Exceptions;
-using Sekiban.Core.Types;
 namespace Sekiban.Core.Query.MultiProjections;
 
 public class MultiProjection<TProjectionPayload> : IMultiProjector<TProjectionPayload>, IMultiProjectionBase
     where TProjectionPayload : IMultiProjectionPayloadCommon, new()
 {
-    private TProjectionPayload Payload { get; set; } = new();
-    public Guid LastEventId { get; set; }
-    public string LastSortableUniqueId { get; set; } = string.Empty;
-    public int AppliedSnapshotVersion { get; set; }
-    public int Version { get; set; }
+    private MultiProjectionState<TProjectionPayload> state = new(new TProjectionPayload(), Guid.Empty, string.Empty, 0, 0);
+    private TProjectionPayload Payload => state.Payload;
+    public Guid LastEventId => state.LastEventId;
+    public string LastSortableUniqueId => state.LastSortableUniqueId;
+    public int AppliedSnapshotVersion => state.AppliedSnapshotVersion;
+    public int Version => state.Version;
 
     public void ApplyEvent(IEvent ev)
     {
@@ -20,36 +20,28 @@ public class MultiProjection<TProjectionPayload> : IMultiProjector<TProjectionPa
         {
             return;
         }
-
-#if NET7_0_OR_GREATER
-        var method = typeof(TProjectionPayload).GetMethod("ApplyEvent");
-        var genericMethod = method?.MakeGenericMethod(ev.GetEventPayloadType());
-        Payload = (TProjectionPayload)(genericMethod?.Invoke(typeof(TProjectionPayload), new object[] { Payload, ev }) ?? Payload);
-#else
-        var method = Payload.GetType().GetMethod("ApplyEventInstance");
-        var genericMethod = method?.MakeGenericMethod(ev.GetEventPayloadType());
-        Payload = (TProjectionPayload)(genericMethod?.Invoke(Payload, new object[] { Payload, ev }) ?? Payload);
-#endif
-        Version++;
-        LastEventId = ev.Id;
-        LastSortableUniqueId = ev.SortableUniqueId;
+        var projectionPayload = ((IMultiProjectionPayload<TProjectionPayload>)Payload).ApplyIEvent(ev) ?? Payload;
+        state = state with
+        {
+            Payload = projectionPayload,
+            LastEventId = ev.Id,
+            LastSortableUniqueId = ev.SortableUniqueId,
+            Version = state.Version + 1
+        };
     }
 
-    public MultiProjectionState<TProjectionPayload> ToState() => new(
-        Payload,
-        LastEventId,
-        LastSortableUniqueId,
-        AppliedSnapshotVersion,
-        Version);
-    public bool EventShouldBeApplied(IEvent ev) => ev.GetSortableUniqueId().LaterThan(new SortableUniqueIdValue(LastSortableUniqueId));
+    public MultiProjectionState<TProjectionPayload> ToState()
+    {
+        return state;
+    }
+    public bool EventShouldBeApplied(IEvent ev)
+    {
+        return ev.GetSortableUniqueId().LaterThan(new SortableUniqueIdValue(LastSortableUniqueId));
+    }
 
     public void ApplySnapshot(MultiProjectionState<TProjectionPayload> snapshot)
     {
-        Version = snapshot.Version;
-        LastEventId = snapshot.LastEventId;
-        LastSortableUniqueId = snapshot.LastSortableUniqueId;
-        AppliedSnapshotVersion = snapshot.Version;
-        Payload = snapshot.Payload;
+        state = snapshot with { AppliedSnapshotVersion = snapshot.Version };
     }
 
     public virtual IList<string> TargetAggregateNames()
