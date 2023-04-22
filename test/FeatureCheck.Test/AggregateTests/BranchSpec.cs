b572b2e6f2d09a70457e141bef186deb14657409
Tomohisa Takaoka <ttakaoka@jtechs.com>
2023-04-21 20:25:15 -0700

    Validation error should print which command.

--- test/FeatureCheck.Test/AggregateTests/BranchSpec.cs
+++ test/FeatureCheck.Test/AggregateTests/BranchSpec.cs
@@ -2,6 +2,7 @@ using FeatureCheck.Domain.Aggregates.Branches;
 using FeatureCheck.Domain.Aggregates.Branches.Commands;
 using FeatureCheck.Domain.Shared;
 using Sekiban.Testing.SingleProjections;
+using System.ComponentModel.DataAnnotations;
 using Xunit;
 namespace FeatureCheck.Test.AggregateTests;
 
@@ -12,5 +13,15 @@ public class BranchSpec : AggregateTest<Branch, FeatureCheckDependency>
     {
         WhenCommand(new CreateBranch("Japan Tokyo"))
             .ThenPayloadIs(new Branch("Japan Tokyo"));
+    }
+    [Fact]
+    public void ValidationErrorTest()
+    {
+        Assert.Throws<ValidationException>(
+            () =>
+            {
+                RunEnvironmentCommand(new CreateBranch("Japan Tokyo99999999999999999999999"));
+            });
+
     }
 }
