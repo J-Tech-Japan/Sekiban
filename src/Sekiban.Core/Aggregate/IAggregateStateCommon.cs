e0bc64abc0e4170b1b4be4a9bfdb1a7ab7799364
Tomohisa Takaoka <ttakaoka@jtechs.com>
2023-07-07 21:51:15 -0700

    working on slash comments

--- src/Sekiban.Core/Aggregate/IAggregateStateCommon.cs
+++ src/Sekiban.Core/Aggregate/IAggregateStateCommon.cs
@@ -1,6 +1,11 @@
 using Sekiban.Core.Query.SingleProjections;
 namespace Sekiban.Core.Aggregate;
 
+/// <summary>
+///     Common Aggregate Interface
+///     Note: Developer does not need to implement this interface.
+///     It will be used with AggregateState and SingleProjectionState
+/// </summary>
 public interface IAggregateStateCommon : IAggregateCommon
 {
     public dynamic GetPayload();
--- src/Sekiban.Core/Aggregate/IAggregateSubtypePayload.cs
+++ src/Sekiban.Core/Aggregate/IAggregateSubtypePayload.cs
@@ -1,5 +1,12 @@
 namespace Sekiban.Core.Aggregate;
 
+/// <summary>
+///     Aggregate Payload for Subtype
+///     You can implement this for your aggregate subtype payload class.
+///     It will always use with <see cref="IParentAggregatePayload{TSelf,TFirst}" />
+///     or <see cref="IParentAggregatePayload{TFirst}" />
+/// </summary>
+/// <typeparam name="TParentAggregatePayload">Parent Aggregate</typeparam>
 public interface IAggregateSubtypePayload<TParentAggregatePayload> : IApplicableAggregatePayload<TParentAggregatePayload>
     where TParentAggregatePayload : IParentAggregatePayloadCommon<TParentAggregatePayload>
 {
