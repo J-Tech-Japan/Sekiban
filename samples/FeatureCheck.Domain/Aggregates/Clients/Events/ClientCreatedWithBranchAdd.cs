676d65cb333a1806c298513e7e06ba82600ea284
Tomohisa Takaoka <ttakaoka@jtechs.com>
2023-07-17 18:47:45 -0700

    fixed bug

--- samples/FeatureCheck.Domain/Aggregates/Clients/Events/ClientCreatedWithBranchAdd.cs
+++ samples/FeatureCheck.Domain/Aggregates/Clients/Events/ClientCreatedWithBranchAdd.cs
@@ -4,9 +4,9 @@ using Sekiban.Core.Events;
 using Sekiban.Core.PubSub;
 namespace FeatureCheck.Domain.Aggregates.Clients.Events;
 
-public record ClientCreatedWithBranchAdd(Guid BranchId, string ClientName, string ClientEmail) : IEventPayload<Client, ClientCreated>
+public record ClientCreatedWithBranchAdd(Guid BranchId, string ClientName, string ClientEmail) : IEventPayload<Client, ClientCreatedWithBranchAdd>
 {
-    public static Client OnEvent(Client aggregatePayload, Event<ClientCreated> ev) =>
+    public static Client OnEvent(Client aggregatePayload, Event<ClientCreatedWithBranchAdd> ev) =>
         new(ev.Payload.BranchId, ev.Payload.ClientName, ev.Payload.ClientEmail);
 
     public class BranchSubscriber : IEventSubscriber<ClientCreatedWithBranchAdd, BranchSubscriber>
