69938b00edbb0b4593c95334810d506ef2c34b3e
Tomohisa Takaoka <ttakaoka@jtechs.com>
2023-07-21 21:44:50 -0700

    Comments for the Events folder

--- src/Sekiban.Core/Events/Event.cs
+++ src/Sekiban.Core/Events/Event.cs
@@ -4,9 +4,16 @@ using Sekiban.Core.Partition;
 using Sekiban.Core.Types;
 namespace Sekiban.Core.Events;
 
+/// <summary>
+///     Event object. This class is used for all types of events.
+///     Use Generics to specify payload type.
+/// </summary>
+/// <typeparam name="TEventPayload"></typeparam>
 public record Event<TEventPayload> : Document, IEvent where TEventPayload : IEventPayloadCommon
 {
-
+    /// <summary>
+    ///     Payload object.
+    /// </summary>
     public TEventPayload Payload { get; init; } = default!;
     public Event()
     {
@@ -25,13 +32,27 @@ public record Event<TEventPayload> : Document, IEvent where TEventPayload : IEve
     ///     Version after applying the aggregate event.
     /// </summary>
     public int Version { get; init; }
-
+    /// <summary>
+    ///     Event Call histories
+    /// </summary>
     public List<CallHistory> CallHistories { get; init; } = new();
-
+    /// <summary>
+    ///     Get Payload object
+    /// </summary>
+    /// <returns></returns>
     public IEventPayloadCommon GetPayload() => Payload;
-
+    /// <summary>
+    ///     Get Event Payload object by Type
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public T? GetPayload<T>() where T : class, IEventPayloadCommon => Payload as T;
-
+    /// <summary>
+    ///     Convert Event to new version
+    ///     Event payload need to implements
+    ///     <see cref="IEventPayloadConvertingTo{TNewEventPayload}" />
+    /// </summary>
+    /// <returns></returns>
     public (IEvent, IEventPayloadCommon) GetConvertedEventAndPayload()
     {
         var payload = GetPayload();
@@ -54,7 +75,10 @@ public record Event<TEventPayload> : Document, IEvent where TEventPayload : IEve
         return (this, payload);
     }
 
-
+    /// <summary>
+    ///     Add current history nd add this event for next step
+    /// </summary>
+    /// <returns></returns>
     public List<CallHistory> GetCallHistoriesIncludesItself()
     {
         var histories = new List<CallHistory>();
@@ -62,10 +86,22 @@ public record Event<TEventPayload> : Document, IEvent where TEventPayload : IEve
         histories.Add(new CallHistory(Id, GetPayload().GetType().Name, string.Empty));
         return histories;
     }
-
+    /// <summary>
+    ///     Generate event object
+    /// </summary>
+    /// <param name="aggregateId"></param>
+    /// <param name="aggregateType"></param>
+    /// <param name="eventPayload"></param>
+    /// <param name="rootPartitionKey"></param>
+    /// <returns></returns>
     public static Event<TEventPayload> GenerateEvent(Guid aggregateId, Type aggregateType, TEventPayload eventPayload, string rootPartitionKey) =>
         new(aggregateId, aggregateType, eventPayload, rootPartitionKey);
-
+    /// <summary>
+    ///     change payload to converted payload and make new event object
+    /// </summary>
+    /// <param name="newPayload"></param>
+    /// <typeparam name="TNewPayload"></typeparam>
+    /// <returns></returns>
     public Event<TNewPayload> ChangePayload<TNewPayload>(TNewPayload newPayload) where TNewPayload : IEventPayloadCommon =>
         new()
         {
--- src/Sekiban.Core/Events/IEventPayloadConvertingTo.cs
+++ src/Sekiban.Core/Events/IEventPayloadConvertingTo.cs
@@ -1,6 +1,14 @@
 namespace Sekiban.Core.Events;
 
+/// <summary>
+///     Event Version converter. App Developer will implement this when Event can be converted to new version.
+/// </summary>
+/// <typeparam name="TNewEventPayload"></typeparam>
 public interface IEventPayloadConvertingTo<out TNewEventPayload> where TNewEventPayload : IEventPayloadCommon
 {
+    /// <summary>
+    ///     Converting Old events to new events.
+    /// </summary>
+    /// <returns></returns>
     TNewEventPayload ConvertTo();
 }
