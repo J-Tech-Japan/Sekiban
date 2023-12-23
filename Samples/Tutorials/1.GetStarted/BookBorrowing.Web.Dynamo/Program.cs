22d598a10ef8580c2212359441f32d049a9fa323
Tomohisa Takaoka <ttakaoka@jtechs.com>
2023-12-22 23:38:08 -0800

    remove unused class

--- Samples/Tutorials/1.GetStarted/BookBorrowing.Web.Dynamo/Program.cs
+++ Samples/Tutorials/1.GetStarted/BookBorrowing.Web.Dynamo/Program.cs
@@ -40,8 +40,3 @@ app.MapGet(
     .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
