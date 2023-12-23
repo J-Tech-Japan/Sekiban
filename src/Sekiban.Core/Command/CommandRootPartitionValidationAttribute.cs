252e55af3a3ee86c297375f9dab05ccbf21e6184
Tomohisa Takaoka <ttakaoka@jtechs.com>
2023-12-23 00:13:32 -0800

    fix regexOption bug

--- src/Sekiban.Core/Command/CommandRootPartitionValidationAttribute.cs
+++ src/Sekiban.Core/Command/CommandRootPartitionValidationAttribute.cs
@@ -21,12 +21,10 @@ public class CommandRootPartitionValidationAttribute : ValidationAttribute
             return new ValidationResult("Root Partition Key only allow a-z, 0-9, -, _ and length 1-36");
         }
 
-        if (Regex.IsMatch(rootPartitionKey, RootPartitionKeyRegexPattern, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250)))
-        {
-            return ValidationResult.Success;
-        }
+        return Regex.IsMatch(rootPartitionKey, RootPartitionKeyRegexPattern, RegexOptions.None, TimeSpan.FromMilliseconds(250))
+            ? ValidationResult.Success
+            : new ValidationResult("Root Partition Key only allow a-z, 0-9, -, _ and length 1-36");
 
-        return new ValidationResult("Root Partition Key only allow a-z, 0-9, -, _ and length 1-36");
     }
 
     public static bool IsValidRootPartitionKey(string rootPartitionKey)
