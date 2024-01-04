337a065c9dbd57489aa41eb5dee4bbae09598777
Takashi Kawae <kawae@jtechs.com>
2024-01-04 16:22:09 +0900

    Fix SekibanAzureOption class property default values to be declared as static readonly fields.

--- src/Sekiban.Infrastructure.Cosmos/SekibanAzureOption.cs
+++ src/Sekiban.Infrastructure.Cosmos/SekibanAzureOption.cs
@@ -1,23 +1,32 @@
+using LanguageExt.TypeClasses;
 using Microsoft.Extensions.Configuration;
 using Sekiban.Core.Setting;
 namespace Sekiban.Infrastructure.Cosmos;
 
 public record SekibanAzureOption
 {
+    public static readonly string CosmosEventsContainerDefaultValue = "events";
+    public static readonly string CosmosEventsContainerDissolvableDefaultValue = "dissolvableevents";
+    public static readonly string CosmosItemsContainerDefaultValue = "items";
+    public static readonly string CosmosItemsContainerDissolvableDefaultValue = "dissolvableitems";
+    public static readonly string CosmosConnectionStringNameDefaultValue = "SekibanCosmos";
+    public static readonly string CosmosDatabaseDefaultValue = "SekibanDb";
+    public static readonly string BlobConnectionStringNameDefaultValue = "SekibanBlob";
+
     public string Context { get; init; } = SekibanContext.Default;
 
-    public string CosmosEventsContainer { get; init; } = "events";
-    public string CosmosEventsContainerDissolvable { get; init; } = "dissolvableevents";
-    public string CosmosItemsContainer { get; init; } = "items";
-    public string CosmosItemsContainerDissolvable { get; init; } = "dissolvableitems";
+    public string CosmosEventsContainer { get; init; } = CosmosEventsContainerDefaultValue;
+    public string CosmosEventsContainerDissolvable { get; init; } = CosmosEventsContainerDissolvableDefaultValue;
+    public string CosmosItemsContainer { get; init; } = CosmosItemsContainerDefaultValue;
+    public string CosmosItemsContainerDissolvable { get; init; } = CosmosItemsContainerDissolvableDefaultValue;
 
     public string? CosmosEndPointUrl { get; init; }
     public string? CosmosAuthorizationKey { get; init; }
-    public string CosmosConnectionStringName { get; init; } = "SekibanCosmos";
+    public string CosmosConnectionStringName { get; init; } = CosmosConnectionStringNameDefaultValue;
     public string? CosmosConnectionString { get; init; }
-    public string CosmosDatabase { get; init; } = "SekibanDb";
+    public string CosmosDatabase { get; init; } = CosmosDatabaseDefaultValue;
 
-    public string BlobConnectionStringName { get; init; } = "SekibanBlob";
+    public string BlobConnectionStringName { get; init; } = BlobConnectionStringNameDefaultValue;
     public string? BlobConnectionString { get; init; }
 
     public bool LegacyPartitions { get; init; }
@@ -32,39 +41,39 @@ public record SekibanAzureOption
         var eventsContainer = azureSection.GetValue<string>(nameof(CosmosEventsContainer))
             ?? azureSection.GetValue<string>("CosmosDbEventsContainer")
             ?? azureSection.GetValue<string>("AggregateEventCosmosDbContainer")
-            ?? "events";
+            ?? CosmosEventsContainerDefaultValue;
         var eventsContainerDissolvable = azureSection.GetValue<string>(nameof(CosmosEventsContainerDissolvable))
             ?? azureSection.GetValue<string>("CosmosDbEventsContainerDissolvable")
             ?? azureSection.GetValue<string>("AggregateEventCosmosDbContainerDissolvable")
-            ?? "dissolvableevents";
+            ?? CosmosEventsContainerDissolvableDefaultValue;
         var itemsContainer = azureSection.GetValue<string>(nameof(CosmosItemsContainer))
             ?? azureSection.GetValue<string>("CosmosDbItemsContainer")
             ?? azureSection.GetValue<string>("CosmosDbContainer")
             ?? azureSection.GetValue<string>("CosmosDbCommandsContainer")
             ?? azureSection.GetValue<string>("AggregateCommandCosmosDbContainer")
-            ?? "items";
+            ?? CosmosItemsContainerDefaultValue;
         var itemsContainerDissolvable = azureSection.GetValue<string>(nameof(CosmosItemsContainerDissolvable))
             ?? azureSection.GetValue<string>("CosmosDbItemsContainerDissolvable")
             ?? azureSection.GetValue<string>("CosmosDbContainerDissolvable")
             ?? azureSection.GetValue<string>("CosmosDbCommandsContainerDissolvable")
             ?? azureSection.GetValue<string>("AggregateCommandCosmosDbContainerDissolvable")
-            ?? "dissolvableitems";
+            ?? CosmosItemsContainerDissolvableDefaultValue;
 
         var cosmosEndPointUrl = azureSection.GetValue<string>(nameof(CosmosEndPointUrl))
             ?? azureSection.GetValue<string>("CosmosDbEndPointUrl");
         var cosmosAuthorizationKey = azureSection.GetValue<string>(nameof(CosmosAuthorizationKey))
             ?? azureSection.GetValue<string>("CosmosDbAuthorizationKey");
         var cosmosConnectionStringName = azureSection.GetValue<string>(nameof(CosmosConnectionStringName))
-            ?? "SekibanCosmos";
+            ?? CosmosConnectionStringNameDefaultValue;
         var cosmosConnectionString = configurationRoot.GetConnectionString(cosmosConnectionStringName)
             ?? section.GetValue<string>(nameof(CosmosConnectionString))
             ?? section.GetValue<string>("CosmosDbConnectionString");
         var cosmosDatabase = azureSection.GetValue<string>(nameof(CosmosDatabase))
             ?? azureSection.GetValue<string>("CosmosDbDatabase")
-            ?? "SekibanDb";
+            ?? CosmosDatabaseDefaultValue;
 
         var blobConnectionStringName = azureSection.GetValue<string>(nameof(BlobConnectionStringName))
-            ?? "SekibanBlob";
+            ?? BlobConnectionStringNameDefaultValue;
         var blobConnectionString = configurationRoot.GetConnectionString(blobConnectionStringName)
             ?? azureSection.GetValue<string>(nameof(BlobConnectionString));
 
