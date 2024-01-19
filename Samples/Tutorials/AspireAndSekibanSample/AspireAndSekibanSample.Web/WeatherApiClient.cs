98f51369ddd7fe49299a1a75f432c4ec5a2b982a
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-01-18 17:45:51 -0800

    Remove unused weather information.

--- Samples/Tutorials/AspireAndSekibanSample/AspireAndSekibanSample.Web/WeatherApiClient.cs
+++ Samples/Tutorials/AspireAndSekibanSample/AspireAndSekibanSample.Web/WeatherApiClient.cs
@@ -6,17 +6,8 @@ namespace AspireAndSekibanSample.Web;
 
 public class WeatherApiClient(HttpClient httpClient)
 {
-    public async Task<WeatherForecast[]> GetWeatherAsync()
-    {
-        return await httpClient.GetFromJsonAsync<WeatherForecast[]>("/weatherforecast") ?? [];
-    }
     public async Task<ListQueryResult<QueryAggregateState<AccountUser>>?> GetAccountUserAsync()
     {
         return await httpClient.GetFromJsonAsync<ListQueryResult<QueryAggregateState<AccountUser>>>("/api/query/accountuser/simpleaggregatelistquery1");
     }
-}
-
-public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
