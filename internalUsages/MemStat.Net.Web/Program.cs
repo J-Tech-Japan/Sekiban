d7701208ad17bb39fe25710bbef2b3a16271983c
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-10-25 22:55:14 -0700

    MemStat release

--- internalUsages/MemStat.Net.Web/Program.cs
+++ internalUsages/MemStat.Net.Web/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMemoryUsageFinder();
-// builder.Services.AddTransient<IMemoryUsageFinder, MemoryUsageFinder>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
