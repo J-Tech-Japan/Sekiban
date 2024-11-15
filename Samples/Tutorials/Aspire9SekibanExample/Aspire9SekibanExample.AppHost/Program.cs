8cac5039c8914ea25bad7a109aeb2c8ef8c75e04
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-11-15 09:14:32 -0800

    improve .NET 9 sample with aspire

--- Samples/Tutorials/Aspire9SekibanExample/Aspire9SekibanExample.AppHost/Program.cs
+++ Samples/Tutorials/Aspire9SekibanExample/Aspire9SekibanExample.AppHost/Program.cs
@@ -1,8 +1,25 @@
+using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.Aspire9SekibanExample_ApiService>("apiservice");
 
-builder.AddProject<Projects.Aspire9SekibanExample_Web>("webfrontend")
+
+var postgresPassword = builder.AddParameter("postgres-password", true);
+var postgres = builder
+    .AddPostgres("aspire9Postgres", password: postgresPassword)
+    .WithDataVolume("aspire9PostgresData")
+    .WithPgAdmin()
+    .AddDatabase("SekibanPostgres");
+
+
+
+var apiService = builder
+    .AddProject<Aspire9SekibanExample_ApiService>("apiservice")
+    .WithReference(postgres)
+    .WithEndpoint("http", annotation => annotation.IsProxied = false)
+    .WithEndpoint("https", annotation => annotation.IsProxied = false);
+
+builder
+    .AddProject<Aspire9SekibanExample_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
     .WaitFor(apiService);
