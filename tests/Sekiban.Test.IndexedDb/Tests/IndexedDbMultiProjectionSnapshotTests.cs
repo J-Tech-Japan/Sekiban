49776f5d24912ce1e8fefeb5dd4b84efc9028cb7
t.miura <miura.takuro@infogram.co.jp>
2024-11-28 17:41:02 +0900

    add testcases

---
+++ tests/Sekiban.Test.IndexedDb/Tests/IndexedDbMultiProjectionSnapshotTests.cs
@@ -0,0 +1,15 @@
+using Sekiban.Infrastructure.IndexedDb;
+using Sekiban.Test.Abstructs.Abstructs;
+using Xunit.Abstractions;
+namespace Sekiban.Test.IndexedDb.Tests;
+
+public class IndexedDbMultiProjectionSnapshotTests : MultiProjectionSnapshotTests
+{
+
+    public IndexedDbMultiProjectionSnapshotTests(SekibanTestFixture sekibanTestFixture, ITestOutputHelper testOutputHelper) : base(
+        sekibanTestFixture,
+        testOutputHelper,
+        new IndexedDbSekibanServiceProviderGenerator())
+    {
+    }
+}
