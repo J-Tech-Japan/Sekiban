e97d9b4d969ac329560e70de42625b49a09186b5
t.miura <miura.takuro@infogram.co.jp>
2024-11-29 21:03:02 +0900

    WIP write event

--- src/Sekiban.Infrastructure.IndexedDb/IndexedDbFactory.cs
+++ src/Sekiban.Infrastructure.IndexedDb/IndexedDbFactory.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Sekiban.Core.Cache;
 using Sekiban.Core.Setting;
@@ -5,14 +6,38 @@ using Sekiban.Core.Setting;
 namespace Sekiban.Infrastructure.IndexedDb;
 
 public class IndexedDbFactory(
-    SekibanIndexedDbOptions options,
+    SekibanIndexedDbOptions dbOptions,
     IMemoryCacheAccessor memoryCache,
-    IServiceProvider serviceProvider
+    IServiceProvider serviceProvider,
+    ISekibanJsRuntime sekibanJsRuntime
 )
 {
     private string SekibanContextIdentifier()
     {
         var sekibanContext = serviceProvider.GetService<ISekibanContext>();
         return sekibanContext?.SettingGroupIdentifier ?? SekibanContext.Default;
+    }
+
+    public async Task<T> DbActionAsync<T>(Func<ISekibanIndexedDbContext, Task<T>> action) =>
+        await action(await GetDbContextAsync());
+
+    public async Task DbActionAsync(Func<ISekibanIndexedDbContext, Task> action) =>
+        await action(await GetDbContextAsync());
+
+    private async Task<ISekibanIndexedDbContext> GetDbContextAsync()
+    {
+        var databaseName = dbOptions.Contexts.Find(m => m.Context == SekibanContextIdentifier())?.DatabaseName ?? SekibanIndexedDbOption.DatabaseNameDefaultValue;
+        var cacheKey = $"indexed-db.context.${databaseName}";
+
+        var dbContext = memoryCache.Cache.Get(cacheKey) as ISekibanIndexedDbContext;
+        if (dbContext is not null)
+        {
+            return dbContext;
+        }
+
+        dbContext = await sekibanJsRuntime.CreateContextAsync(databaseName);
+        memoryCache.Cache.Set(cacheKey, dbContext);
+
+        return dbContext;
     }
 }
