e451c254afd08b8a9813f1ec55f42b4d96876a9e
t.miura <miura.takuro@infogram.co.jp>
2025-01-08 18:55:04 +0900

    fix init argument

--- src/Sekiban.Infrastructure.IndexedDb/Databases/BlazorJsRuntime.cs
+++ src/Sekiban.Infrastructure.IndexedDb/Databases/BlazorJsRuntime.cs
@@ -7,7 +7,7 @@ public class BlazorJsRuntime(IJSRuntime jsRuntime) : ISekibanJsRuntime
     public async Task<ISekibanIndexedDbContext> CreateContextAsync(string context)
     {
         var module = await jsRuntime.InvokeAsync<IJSObjectReference>("import", ISekibanJsRuntime.RuntimePath);
-        var store = await module.InvokeAsync<IJSObjectReference>("init");
+        var store = await module.InvokeAsync<IJSObjectReference>("init", context);
         return new BlazorIndexedDbContext(store);
     }
 }
