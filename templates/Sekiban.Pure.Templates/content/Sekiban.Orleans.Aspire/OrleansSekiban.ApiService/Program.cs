11c7c4ac974ab1357b26100d8499ca8b72049472
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-02-24 00:18:50 -0800

    Remove placeholder files and add initial components and configuration for Sekiban Orleans Aspire template

---
+++ templates/Sekiban.Pure.Templates/content/Sekiban.Orleans.Aspire/OrleansSekiban.ApiService/Program.cs
@@ -0,0 +1,108 @@
+using Microsoft.AspNetCore.Mvc;
+using OrleansSekiban.Domain;
+using OrleansSekiban.Domain.Generated;
+using ResultBoxes;
+using Scalar.AspNetCore;
+using Sekiban.Pure.AspNetCore;
+using Sekiban.Pure.Command.Handlers;
+using Sekiban.Pure.CosmosDb;
+using Sekiban.Pure.Orleans.Parts;
+using Sekiban.Pure.Postgres;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service defaults & Aspire client integrations.
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddProblemDetails();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.AddKeyedAzureTableClient("orleans-sekiban-clustering");
+builder.AddKeyedAzureBlobClient("orleans-sekiban-grain-state");
+builder.UseOrleans(
+    config =>
+    {
+        config.UseDashboard(options => { });
+        config.AddMemoryStreams("EventStreamProvider").AddMemoryGrainStorage("EventStreamProvider");
+    });
+
+builder.Services.AddSingleton(
+    OrleansSekibanDomainDomainTypes.Generate(OrleansSekibanDomainEventsJsonContext.Default.Options));
+
+SekibanSerializationTypesChecker.CheckDomainSerializability(OrleansSekibanDomainDomainTypes.Generate());
+
+builder.Services.AddTransient<ICommandMetadataProvider, CommandMetadataProvider>();
+builder.Services.AddTransient<IExecutingUserProvider, HttpExecutingUserProvider>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddTransient<SekibanOrleansExecutor>();
+
+
+
+if (builder.Configuration.GetSection("Sekiban").GetValue<string>("Database")?.ToLower() == "cosmos")
+{
+    // Cosmos settings
+    builder.AddSekibanCosmosDb();
+} else
+{
+    // Postgres settings
+    builder.AddSekibanPostgresDb();
+}
+
+var app = builder.Build();
+
+var apiRoute = app
+    .MapGroup("/api")
+    .AddEndpointFilter<ExceptionEndpointFilter>();
+
+// Configure the HTTP request pipeline.
+app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+
+apiRoute.MapGet("/weatherforecast", async ([FromServices]SekibanOrleansExecutor executor) =>
+    {
+        var list = await executor.QueryAsync(new WeatherForecastQuery("")).UnwrapBox();
+        return list.Items;
+}).WithOpenApi()
+.WithName("GetWeatherForecast");
+
+apiRoute
+    .MapPost(
+        "/inputweatherforecast",
+        async (
+            [FromBody] InputWeatherForecastCommand command,
+            [FromServices] SekibanOrleansExecutor executor) => await executor.CommandAsync(command).UnwrapBox())
+    .WithName("InputWeatherForecast")
+    .WithOpenApi();
+
+apiRoute
+    .MapPost(
+        "/removeweatherforecast",
+        async (
+            [FromBody] RemoveWeatherForecastCommand command,
+            [FromServices] SekibanOrleansExecutor executor) => await executor.CommandAsync(command).UnwrapBox())
+    .WithName("RemoveWeatherForecast")
+    .WithOpenApi();
+
+apiRoute
+    .MapPost(
+        "/updateweatherforecastlocation",
+        async (
+            [FromBody] UpdateWeatherForecastLocationCommand command,
+            [FromServices] SekibanOrleansExecutor executor) => await executor.CommandAsync(command).UnwrapBox())
+    .WithName("UpdateWeatherForecastLocation")
+    .WithOpenApi();
+
+app.MapDefaultEndpoints();
+
+app.Run();
