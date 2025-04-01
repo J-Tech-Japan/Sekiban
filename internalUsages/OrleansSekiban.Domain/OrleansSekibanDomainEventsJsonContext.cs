9801121f473627f706b030388f27d7d750f970ca
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-03-31 21:56:43 -0700

    feat: add JSON serialization support for EmptyAggregatePayload and WeatherForecast types

--- internalUsages/OrleansSekiban.Domain/OrleansSekibanDomainEventsJsonContext.cs
+++ internalUsages/OrleansSekiban.Domain/OrleansSekibanDomainEventsJsonContext.cs
@@ -1,18 +1,28 @@
 using System.Text.Json.Serialization;
 using OrleansSekiban.Domain.Aggregates.WeatherForecasts.Events;
+using OrleansSekiban.Domain.ValueObjects;
+using Sekiban.Pure.Aggregates;
 using Sekiban.Pure.Events;
+using Sekiban.Pure.Projectors;
 
 namespace OrleansSekiban.Domain;
 
 [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
 [JsonSerializable(typeof(EventDocumentCommon))]
 [JsonSerializable(typeof(EventDocumentCommon[]))]
+[JsonSerializable(typeof(EmptyAggregatePayload))]
+[JsonSerializable(typeof(WeatherForecast))]
+[JsonSerializable(typeof(TemperatureCelsius))]
 [JsonSerializable(typeof(EventDocument<WeatherForecastInputted>))]
 [JsonSerializable(typeof(WeatherForecastInputted))]
 [JsonSerializable(typeof(EventDocument<WeatherForecastDeleted>))]
 [JsonSerializable(typeof(WeatherForecastDeleted))]
 [JsonSerializable(typeof(EventDocument<WeatherForecastLocationUpdated>))]
 [JsonSerializable(typeof(WeatherForecastLocationUpdated))]
+[JsonSerializable(typeof(IMultiProjectorCommon))]
+[JsonSerializable(typeof(MultiProjectionState<>))]
+[JsonSerializable(typeof(Dictionary<string, Aggregate>))]
+[JsonSerializable(typeof(Aggregate))]
 public partial class OrleansSekibanDomainEventsJsonContext : JsonSerializerContext
 {
 }
