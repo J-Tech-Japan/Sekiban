6b763b3107dc03146c6651610db1a457c22a2049
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-06-30 16:57:46 -0700

    fix: Clear SchedulerHostAddress in Dapr configuration for improved flexibility

--- internalUsages/DaprSample/DaprSample.AppHost/Program.cs
+++ internalUsages/DaprSample/DaprSample.AppHost/Program.cs
@@ -23,7 +23,7 @@ builder.AddProject<Projects.DaprSample_Api>("api")
         DaprHttpPort = 3501,
         DaprGrpcPort = 50002,
         PlacementHostAddress = "localhost:50005",
-        SchedulerHostAddress = "localhost:50006",
+        SchedulerHostAddress = "",
         ResourcesPaths = [Path.Combine(builder.Environment.ContentRootPath, "..", "dapr-components")]
     });
 
