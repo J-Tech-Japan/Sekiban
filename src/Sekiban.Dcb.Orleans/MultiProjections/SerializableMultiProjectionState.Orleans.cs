c9dd884a1edb3c2ab54dd5c9132211e59f7d704d
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-08-21 20:35:06 -0700

    chore(orleans): remove obsolete SerializableMultiProjectionState surrogate & codec files now replaced by DTO

--- src/Sekiban.Dcb.Orleans/MultiProjections/SerializableMultiProjectionState.Orleans.cs
+++
@@ -1,5 +0,0 @@
-// (deprecated) Former Orleans surrogate + converter for SerializableMultiProjectionState.
-// Kept as inert marker to avoid re-introducing Orleans serialization for core type.
-namespace Sekiban.Dcb.Orleans.MultiProjections;
-
-internal static class SerializableMultiProjectionStateOrleansRemoved { }
