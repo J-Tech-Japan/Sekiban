f19ab6ddccd0523e666f9c0c7b9a06b218d42d20
t.miura <miura.takuro@infogram.co.jp>
2025-10-17 20:55:49 +0900

    extend index usage

--- src/Sekiban.Infrastructure.IndexedDb/Runtime/src/index.ts
+++ src/Sekiban.Infrastructure.IndexedDb/Runtime/src/index.ts
@@ -50,10 +50,39 @@ const filterEvents = async (
 	store: "events" | "dissolvable-events",
 	query: DbEventQuery,
 ): Promise<DbEvent[]> => {
-	const shard =
-		query.PartitionKey !== null
-			? await idb.getAllFromIndex(store, "PartitionKey", query.PartitionKey)
-			: await idb.getAll(store);
+	const shard: DbEvent[] = await (() => {
+		if (query.SortableIdStart !== null && query.SortableIdEnd !== null) {
+			const key = IDBKeyRange.bound(
+				query.SortableIdStart,
+				query.SortableIdEnd,
+				true,
+				true,
+			);
+			return idb.getAllFromIndex(store, "SortableUniqueId", key);
+		}
+
+		if (query.SortableIdStart !== null) {
+			const key = IDBKeyRange.lowerBound(query.SortableIdStart, true);
+			return idb.getAllFromIndex(store, "SortableUniqueId", key);
+		}
+
+		if (query.SortableIdEnd !== null) {
+			const key = IDBKeyRange.upperBound(query.SortableIdEnd, true);
+			return idb.getAllFromIndex(store, "SortableUniqueId", key);
+		}
+
+		if (query.PartitionKey !== null) {
+			const key = IDBKeyRange.only(query.PartitionKey);
+			return idb.getAllFromIndex(store, "PartitionKey", key);
+		}
+
+		if (query.RootPartitionKey !== null) {
+			const key = IDBKeyRange.only(query.RootPartitionKey);
+			return idb.getAllFromIndex(store, "RootPartitionKey", key);
+		}
+
+		return idb.getAll(store);
+	})();
 
 	const items = shard
 		.filter(
@@ -93,10 +122,14 @@ const filterBlobs = async (
 		| "multi-projection-events-blobs",
 	query: DbBlobQuery,
 ): Promise<DbBlob[]> => {
-	const shard =
-		query.Name !== null
-			? await idb.getAllFromIndex(store, "Name", query.Name)
-			: await idb.getAll(store);
+	const shard: DbBlob[] = await (() => {
+		if (query.Name !== null) {
+			const key = IDBKeyRange.only(query.Name);
+			return idb.getAllFromIndex(store, "Name", key);
+		}
+
+		return idb.getAll(store);
+	})();
 
 	const items = shard.toSorted(asc((x) => x.Id));
 
@@ -134,14 +167,24 @@ const operations = (idb: SekibanDb) => {
 	const getCommandsAsync = async (
 		query: DbCommandQuery,
 	): Promise<DbCommand[]> => {
-		const shard =
-			query.PartitionKey !== null
-				? await idb.getAllFromIndex(
-						"commands",
-						"PartitionKey",
-						query.PartitionKey,
-					)
-				: await idb.getAll("commands");
+		const shard: DbCommand[] = await (() => {
+			if (query.SortableIdStart != null) {
+				const key = IDBKeyRange.lowerBound(query.SortableIdStart, true);
+				return idb.getAllFromIndex("commands", "SortableUniqueId", key);
+			}
+
+			if (query.PartitionKey !== null) {
+				const key = IDBKeyRange.only(query.PartitionKey);
+				return idb.getAllFromIndex("commands", "PartitionKey", key);
+			}
+
+			if (query.AggregateContainerGroup !== null) {
+				const key = IDBKeyRange.lowerBound(query.AggregateContainerGroup, true);
+				return idb.getAllFromIndex("commands", "AggregateContainerGroup", key);
+			}
+
+			return idb.getAll("commands");
+		})();
 
 		const items = shard
 			.filter(
@@ -181,14 +224,72 @@ const operations = (idb: SekibanDb) => {
 			return item !== undefined ? [item] : [];
 		}
 
-		const shard =
-			query.PartitionKey !== null
-				? await idb.getAllFromIndex(
-						"single-projection-snapshots",
-						"PartitionKey",
-						query.PartitionKey,
-					)
-				: await idb.getAll("single-projection-snapshots");
+		const shard: DbSingleProjectionSnapshot[] = await (() => {
+			if (query.AggregateId !== null) {
+				const key = IDBKeyRange.only(query.AggregateId);
+				return idb.getAllFromIndex(
+					"single-projection-snapshots",
+					"AggregateId",
+					key,
+				);
+			}
+
+			if (query.AggregateContainerGroup !== null) {
+				const key = IDBKeyRange.only(query.AggregateContainerGroup);
+				return idb.getAllFromIndex(
+					"single-projection-snapshots",
+					"AggregateContainerGroup",
+					key,
+				);
+			}
+
+			if (query.PartitionKey !== null) {
+				const key = IDBKeyRange.only(query.PartitionKey);
+				return idb.getAllFromIndex(
+					"single-projection-snapshots",
+					"PartitionKey",
+					key,
+				);
+			}
+
+			if (query.RootPartitionKey !== null) {
+				const key = IDBKeyRange.only(query.RootPartitionKey);
+				return idb.getAllFromIndex(
+					"single-projection-snapshots",
+					"RootPartitionKey",
+					key,
+				);
+			}
+
+			if (query.AggregateType !== null) {
+				const key = IDBKeyRange.only(query.AggregateType);
+				return idb.getAllFromIndex(
+					"single-projection-snapshots",
+					"AggregateType",
+					key,
+				);
+			}
+
+			if (query.PayloadVersionIdentifier !== null) {
+				const key = IDBKeyRange.only(query.PayloadVersionIdentifier);
+				return idb.getAllFromIndex(
+					"single-projection-snapshots",
+					"PayloadVersionIdentifier",
+					key,
+				);
+			}
+
+			if (query.SavedVersion !== null) {
+				const key = IDBKeyRange.only(query.SavedVersion);
+				return idb.getAllFromIndex(
+					"single-projection-snapshots",
+					"SavedVersion",
+					key,
+				);
+			}
+
+			return idb.getAll("single-projection-snapshots");
+		})();
 
 		const items = shard
 			.filter(
@@ -237,14 +338,36 @@ const operations = (idb: SekibanDb) => {
 	const getMultiProjectionSnapshotsAsync = async (
 		query: DbMultiProjectionSnapshotQuery,
 	): Promise<DbMultiProjectionSnapshot[]> => {
-		const shard =
-			query.PartitionKey !== null
-				? await idb.getAllFromIndex(
-						"multi-projection-snapshots",
-						"PartitionKey",
-						query.PartitionKey,
-					)
-				: await idb.getAll("multi-projection-snapshots");
+		const shard: DbMultiProjectionSnapshot[] = await (() => {
+			if (query.PartitionKey !== null) {
+				const key = IDBKeyRange.only(query.PartitionKey);
+				return idb.getAllFromIndex(
+					"multi-projection-snapshots",
+					"PartitionKey",
+					key,
+				);
+			}
+
+			if (query.AggregateContainerGroup !== null) {
+				const key = IDBKeyRange.only(query.AggregateContainerGroup);
+				return idb.getAllFromIndex(
+					"multi-projection-snapshots",
+					"AggregateContainerGroup",
+					key,
+				);
+			}
+
+			if (query.PayloadVersionIdentifier !== null) {
+				const key = IDBKeyRange.only(query.PayloadVersionIdentifier);
+				return idb.getAllFromIndex(
+					"multi-projection-snapshots",
+					"PayloadVersionIdentifier",
+					key,
+				);
+			}
+
+			return idb.getAll("multi-projection-snapshots");
+		})();
 
 		const items = shard
 			.filter(
